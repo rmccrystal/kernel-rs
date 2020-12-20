480ceab9c66f6d1ce72a8e369fe695c63abdf992
Ryan McCrystal <rrmccrystal@gmail.com>
2020-12-19 15:25:19 -1000

    Update hook.rs

--- src/kernel/hook.rs
+++ src/kernel/hook.rs
@@ -8,13 +8,13 @@ pub unsafe fn hook_function(address: *mut c_void, hook_fn: unsafe fn(*mut c_void
     let jmp_rax = [ 0xFF_u8, 0xE0 ];
 
     // let mut original_fn = [ 0x00_u8, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 ];
-    let mut original_fn: [u8; 12] = mem::zeroed();
+    let mut opcodes: [u8; 12] = mem::zeroed();
 
-    original_fn[0..=1].clone_from_slice(&mov_rax);
-    original_fn[2..=9].clone_from_slice(&(hook_fn as *const () as u64).to_le_bytes());
-    original_fn[10..=11].clone_from_slice(&jmp_rax);
+    opcodes[0..=1].clone_from_slice(&mov_rax);
+    opcodes[2..=9].clone_from_slice(&(hook_fn as *const () as u64).to_le_bytes());
+    opcodes[10..=11].clone_from_slice(&jmp_rax);
 
-    if let Err(e) = safe_copy(original_fn.as_ptr(), address as _, mem::size_of_val(&original_fn)) {
+    if let Err(e) = safe_copy(opcodes.as_ptr(), address as _, mem::size_of_val(&opcodes)) {
         error!("Error copying hook: {:?}", e);
     }
 }
