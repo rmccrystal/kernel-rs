0cd123356acde82147bd34c031f59b6b56f5d44e
Ryan McCrystal <rrmccrystal@gmail.com>
2021-01-14 22:16:06 -1000

    Update alloc.rs

--- src/util/alloc.rs
+++ src/util/alloc.rs
@@ -1,7 +1,7 @@
 use core::alloc::{GlobalAlloc, Layout};
 
 
-use crate::include::{_POOL_TYPE_NonPagedPool, ExAllocatePoolWithTag, ExFreePoolWithTag};
+use crate::include::{ExAllocatePoolWithTag, ExFreePoolWithTag, _POOL_TYPE};
 
 static ALLOC_TAG: u32 = unsafe { core::mem::transmute(*b"test") };
 
@@ -10,7 +10,7 @@ pub struct KernelAlloc;
 
 unsafe impl GlobalAlloc for KernelAlloc {
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-        let pool = ExAllocatePoolWithTag(_POOL_TYPE_NonPagedPool, layout.size() as _, ALLOC_TAG);
+        let pool = ExAllocatePoolWithTag(_POOL_TYPE::NonPagedPool, layout.size() as _, ALLOC_TAG);
 
         if pool.is_null() {
             panic!("[kernel-alloc] failed to allocate pool.");
